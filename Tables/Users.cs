5d97928f5e93d4039815589c4fae9ac02fe80278
HajizadaIlgar <ilgarh-ab108@code.edu.az>
2024-11-19 02:23:58 +0400

    Add New Project

---
+++ Tables/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCore.Tables;
+
+public class Users
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null;
+    public string Surname { get; set; } = null;
+    public string Username { get; set; } = null;
+    public string Password { get; set; } = null;
+
+}
+//Id, Name, Surname, Username, Password
