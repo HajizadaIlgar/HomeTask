d1f669b101dd26f5e3cea3918ad55bef028daef7
HajizadaIlgar <ilgarh-ab108@code.edu.az>
2024-11-20 04:01:39 +0400

    Add Db First

---
+++ MiniProject/Controllers/TaskController1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniProject.model;
+
+namespace MiniProject.Controllers
+{
+    public class TaskController1 : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using (MiniClassRoomContext _context = new MiniClassRoomContext())
+            {
+                return View(await _context.DoingTasks.ToListAsync());
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DoingTask data)
+        {
+            using (MiniClassRoomContext _con = new MiniClassRoomContext())
+            {
+                await _con.DoingTasks.AddAsync(data);
+                await _con.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MiniProject/model/DoingTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniProject.model;
+
+public partial class DoingTask
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime? DeedLine { get; set; }
+}
